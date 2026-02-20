6dd612c65c6d372842b864c19aea8ebfce5c6240
Fabiano Franca <Fabiano.Franca@fies.org.br>
2026-02-20 11:41:51 -0300

    Add migration colunm destaque

---
+++ src/BikeRoubada.Data/Migrations/20260220144117_Add-destaque.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeRoubada.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adddestaque : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Destaque",
+                table: "Arquivos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Destaque",
+                table: "Arquivos");
+        }
+    }
+}
